1c0591633d5c4cf938a80e08dd96dcd91db8591a
19Kailane <adelciamiguel@gmail.com>
2025-10-08 09:44:40 +0100

    laboratório4

---
+++ main.js
@@ -0,0 +1,15 @@
+function saudar(){
+    const h1 = document.querySelector('h1');
+    const b = document.querySelector('button'); 
+    const p1 = document.querySelector('p1');  
+      
+    if(frase.textContent==="Bem vindo a minha pagina"){
+        frase.textContent= 'Goodbye';
+        frase.style.color='red';
+        }
+        else{
+            frase.textContent="Bem vindo a minha pagina";
+            frase.style.color='black';
+        }
+    }
+    handler = document.querySelector('p'); 
